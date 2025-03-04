d4e9c18ea95e27f9abf7ff5894aad9ef54ea12b8
Yuanzhen Zhu <47734593+DiracZhu1998@users.noreply.github.com>
2025-03-04 11:08:48 +0000

    Add files via upload

---
+++ 0.atlas/6.SAMap/SAMap_run_vertebrate_neurons.py
@@ -0,0 +1,49 @@
+from samap.mapping import SAMAP
+from samap.analysis import (get_mapping_scores, GenePairFinder,
+    sankey_plot, chord_plot, CellTypeTriangles,
+    ParalogSubstitutions, FunctionalEnrichment,
+    convert_eggnog_to_homologs, GeneTriangles)
+from samalg import SAM
+import pandas as pd
+import random
+
+random.seed(42)
+
+fn1 = '/data/biol-brain-scSEQ/ball6395/05.SAMap_comparison/0.bin/02.atlas/Hsap.neurons_pr.h5ad'
+fn2 = '/data/biol-brain-scSEQ/ball6395/05.SAMap_comparison/0.bin/02.atlas/Mmus.neurons_pr.h5ad'
+fn3 = '/data/biol-brain-scSEQ/ball6395/05.SAMap_comparison/0.bin/02.atlas/Pvit.neurons_pr.h5ad'
+fn4 = '/data/biol-brain-scSEQ/ball6395/05.SAMap_comparison/0.bin/02.atlas/Pmar.neurons_pr.h5ad'
+
+sam1=SAM()
+sam1.load_data(fn1)
+sam2=SAM()
+sam2.load_data(fn2)
+sam3=SAM()
+sam3.load_data(fn3)
+sam4=SAM()
+sam4.load_data(fn4)
+
+sams = {'hs':sam1,'mm':sam2,'pv':sam3,'pm':sam4}
+sm = SAMAP(
+        sams,
+        f_maps = './maps/',
+        keys = {'hs':'cell_type','mm':'cell_type','pv':'cell_type','pm':'cell_type'},
+        save_processed=False #if False, do not save the processed results to `*_pr.h5ad`
+)
+
+
+sm.run(
+        pairwise=True,
+        NUMITERS = 5,
+        crossK = 30,
+        ncpus = 8,
+        hom_edge_mode = "pearson",
+        N_GENE_CHUNKS = 8
+        )
+#sm.run(neigh_from_keys = {'hs':True,'mm':True,'pv':True}) #
+#samap = sm.samap # SAM object with three species stitched togetherfilenamesfilenames
+
+import pickle
+with open('vertebrate_neurons.pkl', 'wb') as f:  # open a text file
+    pickle.dump(sm, f)
+f.close()
